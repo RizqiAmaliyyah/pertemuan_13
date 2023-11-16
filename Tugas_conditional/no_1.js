72b9a10efce41524ac82a5824301741b2852a690
RizqiAmaliyyah <rizqia439@gmail.com>
2023-11-17 01:01:30 +0700

    kedua

--- Tugas_conditional/no_1.js
+++ Tugas_conditional/no_1.js
@@ -2,8 +2,14 @@
 // Contoh: Jika saya sebagai HR maka tugas saya adalah melakukan rekrutmen untuk calon pegawai baru
 
 
-let HR = true;
-if(HR) {
+let posisi = 'HR';
+
+if(posisi == 'HR') {
   console.log('Jika saya sebagai HR maka tugas saya adalah melakukan rekrutmen untuk calon pegawai baru');
+} else if (posisi === 'PM') {
+  console.log('Jika saya sebagai PM maka tugas saya adalah melakukan rapat dengan satekholder');
+} else if (posisi === "Dev") {
+  console.log('Jika saya sebagai Dev maka tugas saya adalah melakukan pembuatan code');
 } else {
+  console.log('Tidak terdaftar');
 }
